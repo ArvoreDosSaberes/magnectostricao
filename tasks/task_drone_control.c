689d81e2374504c7ddc14d54d27cfc51023d52d9
Carlos Delfino <consultoria@carlosdelfino.eti.br>
2025-02-08 20:25:59 -0300

    Melhorias no código, ajustes na proposta do cntrole de drone.

--- tasks/task_drone_control.c
+++ tasks/task_drone_control.c
@@ -17,6 +17,16 @@
 #include "tasks_paramiters.h"
 
 /**
+ * Envia aceleração para o drone através da porta I2C.
+ */
+static void send_drone_direction(int x, int y);
+
+// flag definido no main, controlado por interrupção do botão B
+extern bool modo_local;
+// as acelerações abaixo são obtidos atráves do servidor restful
+extern int aceleration_x;
+extern int aceleration_y;
+/**
  * @brief Controls the drone operations.
  *
  * @details This task is responsible for managing drone-related activities.
@@ -34,12 +44,31 @@ void task_drone_control(void *pvParameters){
     TickType_t xLastWakeTime = xTaskGetTickCount();
     while (1)    
     {
-        // Implementar código que recebe os comandos do computador de borda via
-        // servidor web.
-        // Estes comandos serão enviados pela GPIO para os motores e sensores do drone
-        // awqui se usa o 
+
+        if(modo_local){
+
+            adc_select_input(0);
+            uint adc_y_raw = adc_read();
+            adc_select_input(1);
+            uint adc_x_raw = adc_read();
+
+            // Display the joystick position something like this:
+            // X: [            o             ]  Y: [              o         ]
+            const uint bar_width = 10;  // aceleração máxima
+            const uint adc_max = (1 << 12) - 1; // amostragem máxima do ADC
+            int aceleracao_x = (adc_x_raw * bar_width / adc_max) - (adc_max / 2);
+            int aceleracao_y = (adc_y_raw * bar_width / adc_max) - (adc_max / 2);
+            send_drone_direction(aceleracao_x, aceleracao_y);
+        }else{
+            send_drone_direction(aceleration_x, aceleration_y);
+        }
 
         //printf("Loop Task Drone Control %d\n", xLastWakeTime);
         xTaskDelayUntil(&xLastWakeTime, TASK_DRONE_CONTROL_DELAY); /* delay 100 ticks */
     }
+}
+
+static void send_drone_direction(int x, int y){
+    // implementar
+    //este código envia via i2c a direção do drone
 }
